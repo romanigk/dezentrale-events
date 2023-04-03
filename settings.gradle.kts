6c709aead2921cdc33504a9e362ab1aab19d9c08
Robert Manigk <p1ng0ut@mailbox.org>
2023-04-03 20:56:41 +0200

    Add generated ktor project

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "space.dezentrale.dezentrale-events"
---
+++ src/main/kotlin/space/dezentrale/plugins/Security.kt
@@ -0,0 +1,7 @@
+package space.dezentrale.plugins
+
+import io.ktor.server.application.*
+
+fun Application.configureSecurity() {
+    
+}
