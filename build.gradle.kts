9a89d8aee2c0dc17f59c28f4ec66558a1592c5bd
Robert Manigk <p1ng0ut@mailbox.org>
2023-05-29 17:00:06 +0200

    Add docker-compose.yml, application.conf and jib-configuration to the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,13 @@ val kotlinVersion: String by project
 val logbackVersion: String by project
 val postgresVersion : String by project
 val h2Version : String by project
+val hikaricpVersion: String by project
+val ehcacheVersion: String by project
 
 plugins {
     kotlin("jvm") version "1.8.22"
     id("io.ktor.plugin") version "2.3.1"
-                id("org.jetbrains.kotlin.plugin.serialization") version "1.8.22"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.22"
 }
 
 group = "space.dezentrale"
@@ -32,6 +34,21 @@ dependencies {
     implementation("io.ktor:ktor-server-auth-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("com.zaxxer:HikariCP:$hikaricpVersion")
+    implementation("org.ehcache:ehcache:$ehcacheVersion")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+}
+
+ktor {
+    docker {
+        jreVersion.set(io.ktor.plugin.features.JreVersion.JRE_17)
+        externalRegistry.set(
+            io.ktor.plugin.features.DockerImageRegistry.dockerHub(
+                appName = provider { "dezentrale-events" },
+                username = providers.environmentVariable("DOCKER_HUB_USERNAME"),
+                password = providers.environmentVariable("DOCKER_HUB_PASSWORD")
+            )
+        )
+    }
 }
