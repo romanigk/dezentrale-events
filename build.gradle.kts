f5a13366df7d8c38a40594d6a8f224ddd6eeb1d7
Robert Manigk <robert.manigk@codecentric.de>
2024-02-08 17:27:51 +0100

    Add OpenAPI definition and Kotlin code generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import io.ktor.plugin.features.*
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
+
 val ktorVersion: String by project
 val kotlinVersion: String by project
 val logbackVersion: String by project
@@ -5,11 +8,14 @@ val postgresVersion: String by project
 val h2Version: String by project
 val hikaricpVersion: String by project
 val ehcacheVersion: String by project
+val exposedVersion: String by project
+val dropwizardMetricsCore: String by project
 
 plugins {
     kotlin("jvm") version "1.9.23"
     id("io.ktor.plugin") version "2.3.9"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23"
+    id("org.openapi.generator") version "7.4.0"
 }
 
 group = "space.dezentrale.prgrnd"
@@ -31,24 +37,99 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
     implementation("org.postgresql:postgresql:$postgresVersion")
     implementation("com.h2database:h2:$h2Version")
-    implementation("io.ktor:ktor-server-auth-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth:$ktorVersion")
+    implementation("io.ktor:ktor-server-auto-head-response:$ktorVersion")
+    implementation("io.ktor:ktor-server-default-headers:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-gson:$ktorVersion")
+    implementation("io.ktor:ktor-server-resources:$ktorVersion")
+    implementation("io.ktor:ktor-server-hsts:$ktorVersion")
+    implementation("io.ktor:ktor-server-compression:$ktorVersion")
+    implementation("io.ktor:ktor-server-metrics:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("io.dropwizard.metrics:metrics-core:$dropwizardMetricsCore")
     implementation("com.zaxxer:HikariCP:$hikaricpVersion")
     implementation("org.ehcache:ehcache:$ehcacheVersion")
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 }
 
 ktor {
     docker {
-         jreVersion.set(JavaVersion.VERSION_17)
+        jreVersion.set(JavaVersion.VERSION_17)
         externalRegistry.set(
-            io.ktor.plugin.features.DockerImageRegistry.dockerHub(
+            DockerImageRegistry.dockerHub(
                 appName = provider { "dezentrale-events" },
                 username = providers.environmentVariable("DOCKER_HUB_USERNAME"),
                 password = providers.environmentVariable("DOCKER_HUB_PASSWORD")
             )
         )
     }
+}
+
+val openApiValidate = openApiValidate {
+    inputSpec.set("$rootDir/src/main/resources/META-INF/openapi/dezentrale-events.yml")
+    recommend.set(true)
+}
+
+val openApiGeneratedSourcesFolder = "${layout.buildDirectory.get()}/generated-sources"
+val deleteSuperfluousGeneratedFiles = tasks.register<Delete>("deleteSuperfluousGeneratedFiles") {
+    group = "space.dezentrale.prgrnd"
+    description = "This task deletes generated source code that is not needed."
+    delete(
+        file("${openApiGeneratedSourcesFolder}/.openapi-generator-ignore"),
+        file("${openApiGeneratedSourcesFolder}/build.gradle"),
+        file("${openApiGeneratedSourcesFolder}/pom.xml"),
+        file("${openApiGeneratedSourcesFolder}/src/main/resources"),
+        file("${openApiGeneratedSourcesFolder}/gradle.properties"),
+        file("${openApiGeneratedSourcesFolder}/README.md"),
+        file("${openApiGeneratedSourcesFolder}/docker-compose.yml"),
+        file("${openApiGeneratedSourcesFolder}/Dockerfile"),
+        file("${openApiGeneratedSourcesFolder}/settings.gradle"),
+        file("${openApiGeneratedSourcesFolder}/.openapi-generator-ignore"),
+    )
+}
+
+val generateDezentraleEventsFromOpenApi =
+    tasks.register<GenerateTask>("generateDezentraleEventsFromOpenApi") {
+        group = "space.dezentrale.prgrnd"
+        description = """This task generates kotlin (ktor) code from an Open API Spec, which you 
+                can find under META_INF/openapi/dezentrale-events.yml."""
+        groupId.set("space.dezentrale.prgrnd")
+        generatorName.set("kotlin-server")
+        inputSpec.set("$rootDir/src/main/resources/META-INF/openapi/dezentrale-events.yml")
+        apiPackage.set("space.dezentrale.events.api")
+        modelPackage.set("space.dezentrale.events.model")
+        outputDir.set(openApiGeneratedSourcesFolder)
+
+        configOptions.set(
+            mapOf(
+                "groupId" to "space.dezentrale.prgrnd",
+                "artifactId" to "dezentrale-events",
+                "artifactVersion" to "0.0.1",
+                "interfaceOnly" to "true",
+                "enumPropertyNaming" to "original",
+                "packageName" to "space.dezentrale.events"
+            )
+        )
+        finalizedBy(deleteSuperfluousGeneratedFiles)
+    }
+
+kotlin {
+    sourceSets {
+        main {
+            listOf(
+                "src/main/kotlin",
+                "${openApiGeneratedSourcesFolder}/src/main/kotlin"
+            )
+        }
+    }
+}
+
+tasks.named("compileKotlin") {
+    dependsOn(generateDezentraleEventsFromOpenApi)
 }
